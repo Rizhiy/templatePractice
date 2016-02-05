9dcc678d563fd4517d018d43c829b9370bbdf1c8
Artem Vasenin <vasart169@gmail.com>
2016-02-05 23:43:14 +0000

    initial commit

---
+++ controller.php
@@ -0,0 +1,8 @@
+<?php
+class Controller{
+  private $model;
+
+  public function __construct($model){
+    $this->model = $model;
+  }
+} ?>
---
+++ model.php
@@ -0,0 +1,8 @@
+<?php
+class Model {
+  public $tstring;
+
+  public function __construct(){
+    $this->tstring = "The string has been loaded through the template.";
+  }
+} ?>
